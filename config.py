904821a8de05daea100bd56e313bb74ec9e7751f
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-10-10 23:22:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ def _getenv(name: str, default: str | None = None, required: bool = False) -> st
         raise RuntimeError(f"Missing required env var: {name}")
     return val
     
-MUST_JOIN_CHANNEL = "@QuickCodesGc"
+MUST_JOIN_CHANNEL = "@vthgrp"
 BOT_TOKEN = _getenv("BOT_TOKEN", required=True)
 ADMIN_IDS = [int(i) for i in _getenv("ADMIN_IDS", "", required=True).replace(" ", "").split(",") if i]
 API_ID = "21377358"
@@ -18,11 +18,7 @@ API_HASH = "e05bc1f4f03839db7864a99dbf72d1cd"
 DATABASE_URL = _getenv("DATABASE_URL", "mongodb+srv://quickcodes:Stalker123@quickcodes.dm6vjhj.mongodb.net/?retryWrites=true&w=majority&appName=QuickCodes")
 
 # Temporasms API
-API_KEY = "f6ba51d5bfa7ae4861968713433255134984"
-PROVIDER_API_KEY = "f6ba51d5bfa7ae4861968713433255134984"
-PROVIDER_BASE_URL = "https://api.temporasms.com/stubs/handler_api.php"
-PROVIDER_ENDPOINT_GET_NUMBER = _getenv("PROVIDER_ENDPOINT_GET_NUMBER", required=True)
-PROVIDER_ENDPOINT_GET_SMS = _getenv("PROVIDER_ENDPOINT_GET_SMS", required=True)
+
 
 PROVIDER_PARAM_APIKEY = _getenv("PROVIDER_PARAM_APIKEY", "api_key")
 PROVIDER_PARAM_SERVICE = _getenv("PROVIDER_PARAM_SERVICE", "service")
