986eec909ebd6a9764ec7df1dcffab3ccd8f58c6
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-10-12 18:14:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ def _getenv(name: str, default: str | None = None, required: bool = False) -> st
     return val
     
 MUST_JOIN_CHANNEL = "@otpgrabers"
+LOG_CHANNEL= "@otpgrabers"
 BOT_TOKEN = _getenv("BOT_TOKEN", required=True)
 ADMIN_IDS = [int(i) for i in _getenv("ADMIN_IDS", "", required=True).replace(" ", "").split(",") if i]
 API_ID = "21377358"
