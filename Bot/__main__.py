a3864fca481b03b243076e82867ecfea6518a1ef
HKMUSIC <himanshu4hk9@gmail.com>
2025-12-08 11:55:07 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,24 +1,2 @@
-import asyncio
-from Bot.bot import dp, bot  # <-- your actual paths
-from aiogram.exceptions import TelegramNetworkError, TelegramAPIError
-
-async def start():
-    while True:
-        try:
-            print("Bot polling started...")
-            await dp.start_polling(bot)
-
-        except TelegramNetworkError as e:
-            print(f"Network error: {e}")
-            await asyncio.sleep(3)
-
-        except TelegramAPIError as e:
-            print(f"Telegram API error: {e}")
-            await asyncio.sleep(3)
-
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            await asyncio.sleep(5)
-
-if __name__ == "__main__":
-    asyncio.run(start())
+from .bot import main
+main()
